84a771f96100d1cb5167ce2c1660d049520a8d7b
TensorTeacher <138009712+TensorTeacher@users.noreply.github.com>
2023-10-03 00:41:42 +0200

    Update endpoint_miner.py

--- endpoint_miner.py
+++ endpoint_miner.py
@@ -1,54 +1,99 @@
+"""
+cat > endpoint_miner.py
+
+"""
+
+# The MIT License (MIT)
+# Copyright © 2023 Yuma Rao
+# Copyright © 2023 Opentensor Foundation
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+import os
+import time
 import argparse
 import bittensor
-from typing import List, Dict
+from typing import List, Dict, Optional
+import traceback
+from datetime import datetime
+
+
 import requests
 import json
-import traceback
-import time
 
-class EndpointMiner( bittensor.BasePromptingMiner ):
+from prompting.baseminer.miner import Miner
+from prompting.protocol import Prompting
 
+
+class EndpointMiner(Miner):
     @classmethod
-    def check_config( cls, config: 'bittensor.Config' ):
-        pass
+    def add_args(cls, parser: argparse.ArgumentParser):
 
-    @classmethod
-    def add_args( cls, parser: argparse.ArgumentParser ):
         parser.add_argument( '--endpoint.verify_token', type=str, help='Auth' )
         parser.add_argument( '--endpoint.url', type=str, help='Endpoint' )
 
+    def config(self) -> "bittensor.Config":
+
+        parser = argparse.ArgumentParser(description="OpenAI Miner Configs")
+        self.add_args(parser)
+        return bittensor.config(parser)
+
+    def __init__(self, *args, **kwargs):
+        super(EndpointMiner, self).__init__(*args, **kwargs)
+        print("Initialized")
+
+
+    def prompt(self, synapse: Prompting) -> Prompting:
+
         
-    def __init__( self ):
-        super( EndpointMiner, self ).__init__()
-        print ( self.config )
-    
-        
-    def forward(self, messages: List[Dict[str, str]]) -> str:
         start_time=time.time()
-        generation = "An error occured with the miner and it is not available now."
+        errored=False
         
+        generation=""
+        
+        error_msg = "None"
         try:
-            for i, item in enumerate(messages):
-                if type(item) == str:
-                    messages[i] = json.loads(item)
+            messages = [
+                {"role": role, "content": message}
+                for role, message in zip(synapse.roles, synapse.messages)
+            ]
             
-            response = requests.post(self.config.endpoint.url, data=json.dumps({"messages": messages, "verify_token":self.config.endpoint.verify_token}), headers= {"Content-Type": "application/json"}, timeout=9.5)
+            print("messages", messages)
+            response = requests.post(self.config.endpoint.url, data=json.dumps({"messages": messages, "verify_token":self.config.endpoint.verify_token}), headers= {"Content-Type": "application/json"}, timeout=12)
             
-            generation = response.json()["response"]
-            
+            generation = response.json()["response"] 
+
         except Exception as e:
             traceback.print_exc()
-            print("Error", e)
-            print("Response", response.json())
-            print("Errored messages", messages,"\n")
+
             
-        print("messages:", messages)
-        print("Generation:", generation)
-        time_to_sleep = 9.5 - (time.time()-start_time)
+
+        time_to_sleep = 9.6 - (time.time()-start_time)
         if time_to_sleep > 0:
             time.sleep(time_to_sleep)
-        return generation
+            
+
+        synapse.completion = generation
+        
+        print("messages:", messages)
+        print("Generation:", generation)
+        return synapse
+
 
 if __name__ == "__main__":
-    bittensor.utils.version_checking()
-    EndpointMiner().run()
+    print("Starting")
+    with EndpointMiner():
+        while True:
+            print("running...", time.time())
+            time.sleep(10)
